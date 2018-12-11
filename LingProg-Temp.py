d3ad3690974670c49eb3d33a0cac73ff3ea8bfd6
FelipeCosta05 <45776656+FelipeCosta05@users.noreply.github.com>
2018-12-11 00:10:11 -0200

    Add files via upload

---
+++ LingProg-Temp.py
@@ -0,0 +1,19 @@
+import dht
+
+=import machine
+
+   d = dht.DHT11(machine.Pin(4))
+
+
+import dht
+
+=import machine
+
+=    d = dht.DHT22(machine.Pin(4))
+
+
+         d.measure()
+
+         d.temperature()
+
+     d.humidity()
